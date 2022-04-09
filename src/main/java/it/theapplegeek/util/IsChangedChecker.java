6972f2c1a8d966dc74a7fdf6975b279e73efce1f
Weibin Xu <weibin.xu@agmsolutions.net>
2022-04-09 03:50:52 +0200

    add exception and utils

---
+++ src/main/java/it/theapplegeek/util/IsChangedChecker.java
@@ -0,0 +1,27 @@
+package it.theapplegeek.util;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class IsChangedChecker {
+
+    public static Boolean isChanged(String oldString, String newString) {
+        return (newString != null &&
+                newString.length() > 0 &&
+                !Objects.equals(newString, oldString)
+        );
+    }
+
+    public static Boolean isChanged(LocalDate oldDate, LocalDate newDate) {
+        return (newDate != null &&
+                !Objects.equals(newDate, oldDate)
+        );
+    }
+
+    public static Boolean isChanged(Long oldNumber, Long newNumber) {
+        return (newNumber != null &&
+                !Objects.equals(newNumber, oldNumber)
+        );
+    }
+
+}
